f2b6bfde6cd198a21b5c0b76415915bba59e48ce
Nimrod Zimerman <zimerman@fastmail.fm>
2017-09-25 19:03:58 +0300

    Extract wait-for-postgres-instances.sh from build-and-run-all.sh.

--- build-and-run-all.sh
+++ build-and-run-all.sh
@@ -12,14 +12,7 @@ pushd $SCRIPT_DIR
 ./postgres-with-ssl/run.sh
 
 # Wait for both to become available.
-docker run \
-	--link postgres-no-ssl \
-	--link postgres-with-ssl \
-	jwilder/dockerize \
-	dockerize \
-		-timeout 30s \
-		-wait tcp://postgres-no-ssl:5432 \
-		-wait tcp://postgres-with-ssl:5432
+./wait-for-postgres-instances.sh
 
 # Run Kong migrations on both databases.
 ./kong/run-migrations-no-ssl.sh
---
+++ wait-for-postgres-instances.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run \
+	--link postgres-no-ssl \
+	--link postgres-with-ssl \
+	jwilder/dockerize \
+	dockerize \
+		-timeout 30s \
+		-wait tcp://postgres-no-ssl:5432 \
+		-wait tcp://postgres-with-ssl:5432
