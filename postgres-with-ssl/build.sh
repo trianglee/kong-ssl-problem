39885d94d5ca9ca92cf43a4beef15cc9f456dcd1
Nimrod Zimerman <zimerman@fastmail.fm>
2017-09-28 04:53:05 +0300

    Create postgres-with-ssl using a certificate chain of length 3.

--- postgres-with-ssl/build.sh
+++ postgres-with-ssl/build.sh
@@ -9,12 +9,51 @@ set -x
 SCRIPT_DIR=$(dirname "$0")
 pushd $SCRIPT_DIR
 
-openssl req -new -x509 -days 7300 -sha256 -out cert.pem       -keyout private.pem       -subj /C=XX/ST=State/L=Locality/O=Organization/OU=OrgUnit/CN=CommonName -nodes
-openssl req -new -x509 -days 7300 -sha256 -out cert-wrong.pem -keyout private-wrong.pem -subj /C=XX/ST=State/L=Locality/O=Organization/OU=OrgUnit/CN=CommonName -nodes
+# Generate single certificate (no chain) -
 
-rm private-wrong.pem
-mv private.pem docker
-cp cert.pem docker
+#openssl req -new -x509 -days 7300 -sha256 -out cert.pem       -keyout private.pem       -subj /C=XX/ST=State/L=Locality/O=Organization/OU=OrgUnit/CN=CommonName -nodes
+#openssl req -new -x509 -days 7300 -sha256 -out cert-wrong.pem -keyout private-wrong.pem -subj /C=XX/ST=State/L=Locality/O=Organization/OU=OrgUnit/CN=CommonName -nodes
+
+#rm private-wrong.pem
+#mv private.pem docker
+#cp cert.pem docker
+
+
+# Generate certificate chain -
+
+# Generate keys -
+openssl ecparam -out key1.pem -name prime256v1 -genkey
+openssl ecparam -out key2.pem -name prime256v1 -genkey
+openssl ecparam -out key3.pem -name prime256v1 -genkey
+
+# cert1 - self-signed -
+openssl req -key key1.pem -new -x509 -days 7300 -sha256 -out cert1.pem -subj /C=XX/ST=State/L=Locality/O=Organization/OU=OrgUnit/CN=Cert1
+
+# cert2 - signed by cert1 -
+openssl req -new -sha256 -key key2.pem -out csr2.pem -subj /C=XX/ST=State/L=Locality/O=Organization/OU=OrgUnit/CN=Cert2
+openssl x509 -sha256 -req -in csr2.pem -out cert2.pem -CA cert1.pem -CAkey key1.pem -CAcreateserial -days 1095
+
+# cert3 - signed by cert2 -
+openssl req -new -sha256 -key key3.pem -out csr3.pem -subj /C=XX/ST=State/L=Locality/O=Organization/OU=OrgUnit/CN=Cert3
+openssl x509 -sha256 -req -in csr3.pem -out cert3.pem -CA cert2.pem -CAkey key2.pem -CAcreateserial -days 1095
+
+# Verify chain -
+cat cert2.pem cert1.pem > cert-chain.pem
+openssl verify -verbose -CApath no-such-dir -CAfile cert-chain.pem cert3.pem
+rm cert-chain.pem
+
+# Copy certificates and keys -
+rm key1.pem
+mv key2.pem docker
+mv key3.pem docker
+cp cert2.pem docker
+mv cert3.pem docker
+rm csr2.pem
+rm csr3.pem
+rm cert1.srl
+rm cert2.srl
+cat cert1.pem cert2.pem > cert-1-2-bundle.pem
+
 
 set +e
 docker rm -f $IMAGE
--- postgres-with-ssl/docker/reconfigure_postgres.sh
+++ postgres-with-ssl/docker/reconfigure_postgres.sh
@@ -2,8 +2,18 @@
 
 cp /tmp/postgresql.conf /var/lib/postgresql/data/postgresql.conf
 cp /tmp/pg_hba.conf     /var/lib/postgresql/data/pg_hba.conf
-cp /tmp/cert.pem        /var/lib/postgresql/data/server.crt
-cp /tmp/private.pem     /var/lib/postgresql/data/server.key
+
+# Self-signed key and certificate -
+#cp /tmp/cert.pem        /var/lib/postgresql/data/server.crt
+#cp /tmp/private.pem     /var/lib/postgresql/data/server.key
+
+# Chained key and certificate -
+# Chain of length 1 -
+#cp /tmp/key2.pem        /var/lib/postgresql/data/server.key
+#cp /tmp/cert2.pem       /var/lib/postgresql/data/server.crt
+# Chain of length 2 -
+cp /tmp/key3.pem        /var/lib/postgresql/data/server.key
+cp /tmp/cert3.pem       /var/lib/postgresql/data/server.crt
 
 chown -R postgres: /var/lib/postgresql/data/
 chmod 0600 /var/lib/postgresql/data/server.key
